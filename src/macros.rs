b84ca3d5b3bab705d31386854a1236d8f295ccbb
WardLordRuby <chad2lax@gmail.com>
2025-03-03 16:53:32 -0600

    Add trace event in general_event_process

--- src/macros.rs
+++ src/macros.rs
@@ -19,15 +19,19 @@
 ///
 /// This macro requries you to implement [`Executor`] on your `ctx`.
 ///
-/// Internally uses tracing's [`error!`] to log any errors.
+/// Internally uses tracing's [`error!`] to log any errors that a user created [`AsyncCallback`] produces.
+/// As well as emitting a [`trace!`] event if the [`InputHook`] is successfully removed after the error.
 ///
 /// This macro internally uses the try operator on an `io::Result<()>`, and contains break points for the
 /// run eval process loop. Requiring the outer scope of to have the same signature, and be called from
 /// within a loop.
 ///
+/// [`AsyncCallback`]: crate::callback::AsyncCallback
 /// [`Executor`]: crate::executor::Executor
+/// [`InputHook`]: crate::line::InputHook
 /// [`Event`]: <https://docs.rs/crossterm/latest/crossterm/event/enum.Event.html>
 /// [`error!`]: <https://docs.rs/tracing/latest/tracing/macro.error.html>
+/// [`trace!`]: <https://docs.rs/tracing/latest/tracing/macro.trace.html>
 #[macro_export]
 macro_rules! general_event_process {
     ($repl:expr, $ctx:expr, $event_result:expr) => {
@@ -37,7 +41,9 @@ macro_rules! general_event_process {
             $crate::EventLoop::AsyncCallback(callback) => {
                 if let Err(err) = callback($repl, $ctx).await {
                     tracing::error!("{err}");
-                    $repl.conditionally_remove_hook($ctx, &err)?;
+                    if $repl.conditionally_remove_hook($ctx, &err)? {
+                        tracing::trace!("Input hook removed after async callback error")
+                    };
                 }
             }
             $crate::EventLoop::TryProcessInput(Ok(user_tokens)) => {
